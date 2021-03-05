7c889b088ec8ecb22c7bcc624ad36b59df5cb09c
Darmandran Sivapathasundram <vairepxe@gmail.com>
2021-03-05 17:25:00 +0800

    Update IO-Board-Input.js
    made changes to accept both server data

--- IO-Board-Input.js
+++ IO-Board-Input.js
@@ -21,39 +21,67 @@ module.exports = function(RED) {
         	var MAC = node.MAC;
             
             if(msg.payload.MAC !== MAC && msg.payload.device !== board) return;
-
+            
             const {device,temp,status} = msg.payload
-            
-            if(state=="BUTTON"){
-                if((input==="any" && msg.payload.deviceShadow.buttonTrigger!=0)|| input == msg.payload.deviceShadow.buttonTrigger){
-                    if(msg.payload.hasOwnProperty("deviceShadow.buttonState")){
-                        msg.payload.deviceShadow.buttonState = msg.payload.deviceShadow.buttonState==="high"? "high":"low"
-                    }
-                    
-                    // msg.payload = {...msg.payload|msg.payload.deviceShadow}
-                }else{
-                    return
-                }  
-            }else{
-
-                if(input==="any"){
-                    var breakIsTrue = false;
-                    for (var i = 1; i <= 5; i++){
-                        if(relayState["RLY"+i] !== msg.payload.deviceShadow.relayState["RLY"+i]){
-                            relayState["RLY"+i] = msg.payload.deviceShadow.relayState["RLY"+i]
-                            breakIsTrue = true;
+            if(msg.serverType === "listening"){
+                if(state=="BUTTON"){
+                    if((input==="any" && msg.payload.deviceShadow.buttonTrigger!=0)|| input == msg.payload.deviceShadow.buttonTrigger){
+                        if(msg.payload.hasOwnProperty("deviceShadow.buttonState")){
+                            msg.payload.deviceShadow.buttonState = msg.payload.deviceShadow.buttonState==="high"? "high":"low"
                         }
-                    }
-                    if(!breakIsTrue){return;}
-
-                }else if(relayState["RLY"+input] !== msg.payload.deviceShadow.relayState["RLY"+input]){
-                    relayState["RLY"+input] = msg.payload.deviceShadow.relayState["RLY"+input]
+                    }else{
+                        return
+                    }  
                 }else{
-                    return
-                }  
-                // msg.payload = {...msg.payload||msg.payload.relayState}
+                    if(input==="any"){
+                        var breakIsTrue = false;
+                        for (var i = 1; i <= 5; i++){
+                            if(relayState["RLY"+i] !== msg.payload.deviceShadow.relayState["RLY"+i]){
+                                relayState["RLY"+i] = msg.payload.deviceShadow.relayState["RLY"+i]
+                                breakIsTrue = true;
+                            }
+                        }
+                        if(!breakIsTrue){return;}
+
+                    }else if(relayState["RLY"+input] !== msg.payload.deviceShadow.relayState["RLY"+input]){
+                        relayState["RLY"+input] = msg.payload.deviceShadow.relayState["RLY"+input]
+                    }else{
+                        return;
+                    }  
+                }
+            }else if(msg.serverType === "createResponce"){
+                if(!msg.payload.hasOwnProperty("result")) return;
+                if(msg.payload.result !== "success") return;
+                const deviceShadow = {relayState : {RLY1:"",RLY2:"",RLY3:"",RLY4:"",RLY5:""}};
+                msg.payload = {...msg.payload,deviceShadow}
+
+                for (var i = 1; i <= 5; i++){
+                    msg.payload.deviceShadow.relayState["RLY"+i] = msg.payload.data["RLY"+i];
+                }
+
+                if(state==="RELAY"){
+                    if(input==="any"){
+                        var breakIsTrue = false;
+                        for (var i = 1; i <= 5; i++){
+                            if(relayState["RLY"+i] !== msg.payload.deviceShadow.relayState["RLY"+i]){
+                                relayState["RLY"+i] = msg.payload.deviceShadow.relayState["RLY"+i]
+                                breakIsTrue = true;
+                            }
+                        }
+                        if(!breakIsTrue){return;}
+
+                    }else if(relayState["RLY"+input] !== msg.payload.deviceShadow.relayState["RLY"+input]){
+                        relayState["RLY"+input] = msg.payload.deviceShadow.relayState["RLY"+input]
+                    }else{
+                        return;
+                    }  
+                }else{
+                    return;
+                }
+            }else{
+                return;
             }
-            
+
             node.send(msg);
         });
     }
